70b80bb460d89097747388bc990f9ad7b4b023b5
Bruno Germano <bruno.germano@gmail.com>
2018-02-14 23:29:08 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # express-exercise
 REST API exercise using Express
+
+## Methods
+
+(running on localhost:8080)
+
+**`GET` /todos**
+
+expected response:
+
+```JSON
+[
+    {
+        "title": "create a project",
+        "done": false
+    },
+    {
+        "title": "writes classes",
+        "done": false
+    },
+    {
+        "title": "create start script",
+        "done": false
+    }
+]
+```
+
+**`POST` /todos**
+
+request body:
+
+```JSON
+{
+	"title": "make some tests",
+	"done": false
+}
+```
+
+expected response:
+
+```JSON
+{
+	"title": "make some tests",
+	"done": false
+}
+```
+
+**`PUT` /todos/3**
+
+request body:
+
+```JSON
+{
+	"title": "make some tests",
+	"done": true
+}
+```
+
+or partial
+
+request body:
+
+```JSON
+{
+	"done": true
+}
+```
+
+expected response:
+
+```JSON
+{
+	"title": "make some tests",
+	"done": true
+}
+```
+
+**`DELETE` /todos/0**
+
+expected response:
+
+```JSON
+[
+    {
+        "title": "writes classes",
+        "done": false
+    },
+    {
+        "title": "create start script",
+        "done": false
+    }
+]
+```
--- src/todos.js
+++ src/todos.js
@@ -28,21 +28,37 @@ class Todos {
 
     post(req, res) {
         const newItem = req.body;
-        items.push(newItem);
-        res.json(newItem);
+        if (newItem.title !== undefined || newItem.done !== undefined) {
+            items.push(newItem);
+            res.json(newItem);
+        } else {
+            res.status(400);
+            res.send('Body not compatible.');
+        }
     }
 
     put(req, res) {
         const index = req.params.index;
-        const item = Object.assign(items[index], req.body);
-        items[index] = item;
-        res.json(item);
+        if (items[index]) {
+            const item = Object.assign(items[index], req.body);
+            items[index] = item;
+            res.json(item);
+        } else {
+            res.status(400);
+            res.send('Index not exists.');
+        }
+        
     }
     
     delete(req, res) {
         const index = req.params.index;
-        items.splice(index, 1);
-        res.json(items);
+        if (items[index]) {
+            items.splice(index, 1);
+            res.json(items);
+        } else {
+            res.status(400);
+            res.send('Index not exists.');
+        }
     }
 
 }
