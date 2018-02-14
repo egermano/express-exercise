aca9c98e5770ed8ee0d0354b3a262db8710ecd84
Bruno Germano <bruno.germano@gmail.com>
2018-02-14 23:07:08 +0000

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const Todos = require('./todos');
 
 class Main {
-    
+    // app
+
+    constructor () {
+        this.app = express();
+        this.app.use(bodyParser.json());
+        
+        this.app.listen(8080, () => {
+            console.log('Start server... listening on 8080');
+
+            this.setupRoutes();
+        });
+    }
+
+    setupRoutes() {
+        new Todos(this.app);
+    }
 }
+
+new Main();
